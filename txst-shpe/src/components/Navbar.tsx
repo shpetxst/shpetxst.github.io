26785498b29fc30c559fca96ea2df2c3c4636e5e
Thomas M <zaz22@txstate.edu>
2024-10-22 21:21:36 -0500

    removed excess react import

--- txst-shpe/src/components/Navbar.tsx
+++ txst-shpe/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Disclosure } from '@headlessui/react';
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
